3812a78861fd74ba9ceeeb670afbd5dfea3720c7
Artur Cordeiro Oudot Choi <3670788@pc179.phys.upmc.fr>
2019-11-26 11:22:14 +0100

    artur mardi

--- Viscek.f90
+++ Viscek.f90
@@ -1,9 +1,9 @@
 module var
   implicit none
 
-  integer :: a, b, L=5
+  integer :: i, j, a, b, L=5
   integer , parameter :: N = 300
-  real :: m, dt, vnorme=0.03 , eta = 0.1
+  real :: m, vnorme=0.03 , eta = 0.1 ,dt=1.0
 
 end module var
 
@@ -16,44 +16,50 @@ end module var
 program Viscek
   use var
   implicit none
-  real ,dimension (N, 2) :: r , v
-  real ,dimension (N) :: theta
+  real ,dimension (N, 2) :: r , v , r_n , v_n
+  real ,dimension (N) :: theta , theta_n
 
-  call init(r,v)
 
+  call init(r,v,theta)
+  do a = 1 , 200                          ! 200 pas de temps dt
+    call evolution(r,theta,theta_n)       ! influence des voisins sur l'angle
+    call deplacement(theta_n,r,r_n,v_n)
+    r=r_n ; v=v_n ; theta=theta_n
+
+    do i=1,N
+      write(11,*) r(i,1), r(i,2) ,v(i,1), v(i,2)
+    end do
+  write(11,*)
+  write(11,*)
+end do
 endprogram Viscek
 
-function distance (x1,y1,x2,y2)
-  use var
-  implicit none
-  real ,intent(in) :: x1,x2,y1,y2
-  real :: d
-  d=sqrt((x1-x2)**2+(y1-y2)**2)
-endfunction distance
 
-subroutine evolution (r,v,theta)
+
+
+subroutine evolution (r,theta,theta_n)
   use var
   implicit none
 
-!_v siginifit voisin ; _n siginifit new list
+                                                        !_v siginifit voisin ; _n siginifit new list
   real ,dimension (N,2), intent(in) :: r
   real ,dimension (N), intent(in) :: theta
   real ,dimension (N), intent(out) :: theta_n
   integer :: n_v
   real :: thet_av , thet_v , D_thet , d
 
-  do i= 1, N                                            ! on fixe la particule i
-    n_vu=0                                              ! on initialise le nb de voisins , 0 avant la boucle j
+  do i= 1, N                                           ! on fixe la particule i
+    n_v=0                                              ! on initialise le nb de voisins , 0 avant la boucle j
     thet_v=0                                           ! de même pour la somme des angles voisins
     do j=1 , N
       if (i /= j) then                                 ! on balaie
-        call distance(r(i,1),r(i,2),r(j,1),r(j,2))
+        call distance(r(i,1),r(i,2),r(j,1),r(j,2),d)
       endif
 
       if (d <=1) then                                  ! i et j sont voisins
         n_v=n_v+1                                      ! ajoute 1 au nb de voisins
-        thet_v=0+theta(j)
-      endif                             ! somme des thetas voisins
+        thet_v=thet_v+theta(j)
+      endif                                            ! somme des thetas voisins
     enddo
 
     thet_av=(thet_v+theta(i))/(n_v+1)                  ! moyenne des thetas en incluant la particule i
@@ -63,25 +69,51 @@ subroutine evolution (r,v,theta)
 end subroutine evolution
 
 
-subroutine init(r,v)
+
+subroutine deplacement (theta_n,r,r_n,v_n)
+  use var
+  implicit none
+  real ,dimension (N,2), intent(in) :: r
+  real ,dimension (N), intent(in) :: theta_n
+  real ,dimension (N,2), intent(out) :: r_n, v_n       ! Utilisation du nouvel angle pour calculer la nouvelle vitesse et position
+
+  do i = 1,N
+    v_n(i,1)=vnorme*cos(theta_n(i))
+    v_n(i,2)=vnorme*sin(theta_n(i))
+    r_n(i,1)=r(i,1)+v_n(i,1)*dt
+    r_n(i,2)=r(i,2)+v_n(i,2)*dt
+  end do
+
+endsubroutine deplacement
+
+
+
+subroutine distance (x1,y1,x2,y2,d)
+  use var
+  implicit none
+  real ,intent(in) :: x1,x2,y1,y2                      ! Norme de la distance entre 2 particules
+  real , intent(out):: d
+  d=sqrt((x1-x2)**2+(y1-y2)**2)
+endsubroutine distance
+
+
+
+subroutine init(r,v,theta)
   use var
   implicit none
   real ,dimension (N,2), intent(inout) :: r, v
-  real ,dimension (N) :: theta
+  real ,dimension (N), intent (inout):: theta
 
-  open (11,file='/users/etu/3670788/PhysNum/viscek/Viscek_Model/positions/pos_0.txt')
-  open (12,file='/users/etu/3670788/PhysNum/viscek/Viscek_Model/vitesses/vit_0.txt')
-  open (13,file='/users/etu/3670788/PhysNum/viscek/Viscek_Model/angles/tet_0.txt')
-
-   ! Initialisation des coposantes des positions et des vitesses 1:x 2:y
+  open (11,file='/users/etu/3670788/PhysNum/viscek/Viscek_Model/positions/pos_v.txt')
+  ! Initialisation des coposantes des positions et des vitesses 1:x 2:y
   do b = 1, N
     r(b,1) = L*rand()
     r(b,2) = L*rand()
     theta(b)= 2*3.151592*rand()
     v(b,1) = vnorme*cos(theta(b))
-    v(b,2) = sqrt(vnorme**2-v(b,1)**2)
-    write(11,*) r(b,1), r(b,2)
-    write(12,*) v(b,1), v(b,2)
-    write(13,*) theta(b)
+    v(b,2) = vnorme*sin(theta(b))
+    write(11,*) r(b,1), r(b,2) ,v(b,1), v(b,2)
   enddo
+  write(11,*)
+  write(11,*)
 end subroutine init
